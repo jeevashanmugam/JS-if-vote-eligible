eba95a1a41768400abec096989cd66544a069c4f
jeevashanmugam <jeevasj2000@gmail.com>
2023-06-29 22:02:34 +0530

    JS-if-vote-eligible

---
+++ main.js
@@ -0,0 +1,8 @@
+var voter_age = parseInt(prompt("Enter your Voter Age:"));
+
+if (voter_age >= 18) {
+    document.write("You are Eligible for Vote.")
+}
+else {
+    document.write("You are Not Eligible for Vote.")
+}
